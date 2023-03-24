03666136542f9c085a67952c6d51b030e6623a2b
Nick Johnson <nlj222@gmail.com>
2023-03-24 14:42:30 -0700

    logout functionality working

--- server/app.py
+++ server/app.py
@@ -21,14 +21,16 @@ class Login(Resource):
     def post(self):
         data = request.get_json()
         user = User.query.filter_by(email=data['email']).first()
-        if user.authenticate(data['password']):
-            print('all good boss')
-            session['user_id'] = user.id
-            return make_response(user.to_dict(), 200)
+        if user:
+            if user.authenticate(data['password']):
+                # print('all good boss')
+                session['user_id'] = user.id
+                # print(session['user_id'])
+                return make_response(user.to_dict(), 200)
+            else:
+                abort(404, 'Login incorrect.')
         else:
             abort(404, 'Login incorrect.')
-
-
 api.add_resource(Login, '/login')
 
 class AuthorizedSession(Resource):
@@ -43,6 +45,7 @@ api.add_resource(AuthorizedSession, '/authorized')
 class Logout(Resource):
     def delete(self):
         session['user_id'] = None
+        # print(session['user_id'])
         return make_response('', 204)
 api.add_resource(Logout, '/logout')
 
