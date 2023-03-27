030d38026325f1295e4a4bc7970bd3965df1baf1
Nick Johnson <nlj222@gmail.com>
2023-03-27 12:51:19 -0700

    profile page now pulls in crime info without errors

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -20,14 +20,30 @@ function Profile({user}) {
         return <h1>loading</h1>
     } else {
         
-        const {name, bio, photo, email, is_admin, crimes} = profile
-        
+        const {name, bio, photo, email, is_admin, crime_list} = profile
+
+        // debugger;
+
         return (
             <>
             <h1>{name}</h1>
             <p>{bio}</p>
             <img src={photo} />
             <p>{email}</p>
+            <div className="crimes">
+                <h3>Crimes:</h3>
+                <ul>
+                    {crime_list.map((crime, index) => (
+                        <li key={index}>{crime.name}
+                            <ul>
+                                <li>Date committed: {crime.date}</li>
+                                <li>Caught: {crime.caught?"True":"False"}</li>
+                                <li>Convicted: {crime.convicted?"True":"False"}</li>
+                            </ul>
+                        </li>
+                    ))}
+                </ul>
+            </div>
             </>
         );
     }
